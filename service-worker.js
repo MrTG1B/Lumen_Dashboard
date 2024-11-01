37e8a9aafb54a212a95247ce4fdf70d40619a3f3
Tirthankar Dasgupta <tirthankardasgupta913913@gmail.com>
2024-11-01 13:12:39 +0530

    updated

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'luma-cache-v1';
+const urlsToCache = [
+    '/index.html',
+    '/dashboard/dashboard.html',
+    '/static/style.css',
+    '/static/script.js',
+    '/dashboard_static/dashboard_style.css',
+    '/dashboard_static/dashboard_script.js',
+];
+
+// Install the service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
